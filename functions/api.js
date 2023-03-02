9fc2c40fddf4d1f51120d7f8c404eceb56329cb0
Nicholas Oyagh <you@example.com>
2023-03-02 14:55:54 -0800

    changes

--- functions/api.js
+++ functions/api.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json({
     users: [
-      "userOne: James Bond",
       "userTwo: Will Smith",
       "userThree: Sara Pain",
       "userFour: Mike Tyson",
